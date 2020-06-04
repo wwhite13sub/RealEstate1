1e8023f3d784e19fa79c9adcdb164e87fffd46ce
Wanda White <wanda.white32@yahoo.com>
2020-06-03 21:53:27 -0500

    Additonal response code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function cancelHome() {
     var txt;
     if (confirm("Are you sure you would like to Exit? Changes may not be saved.")) {
-        
+        window.location = "index.html"
     }
 }
