f4408b9028f6b4c0171088fbc45da738462054b6
Wanda White <wanda.white32@yahoo.com>
2020-06-06 21:34:37 -0500

    updated code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function myFunction() {
     var x = document.getElementById("firstNumber").value;
-    x = x * 50;    
+    x = x * 50; 
+    if (isNaN(x)) {
+        return "";
+    }
     document.getElementById("result").innerHTML = x;           
 }
